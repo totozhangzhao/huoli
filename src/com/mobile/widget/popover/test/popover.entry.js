a84a0304ec7cf5ee8fa75408beccc62ce957eb26
包义德 <baoyd@133.cn>
2016-02-22 17:01:34 +0800

    ui: popover -- goods detail page

--- src/com/mobile/widget/popover/test/popover.entry.js
+++ src/com/mobile/widget/popover/test/popover.entry.js
@@ -4,18 +4,27 @@ var Popover = require("com/mobile/widget/popover/popover.js");
   更新model 只支持修改文案和方法
   暂不支持修改弹窗的类型
 */
-var popover = new Popover({
-  el: "#popover-box",
-  options: {
-    type: "confirm", // 暂时有 confirm, alert 两种类型 
-    title: "标题",
-    message: "消息",
-    btnText: "确定",  
-    cancleText: "取消", // alert类型 此参数无效
-    clickBtn: function (){ window.alert("clickBtn")},
-    clickCancel: function () { window.alert("clickCancel")} // alert类型 此方法无效
+var alert = new Popover({
+  type: "alert",
+  title: "标题",
+  message: "消息",
+  agreeText: "查看订单",  
+  agreeFunc: function() {
+    window.console.log("alert: agree");
   }
 });
-popover.show();
+
+var pop = new Popover({
+  // el: "#popover-box", // 可选
+  type: "confirm", // 暂时有 confirm, alert 两种类型 
+  title: "标题",
+  message: "消息",
+  agreeText: "确定",  
+  cancelText: "取消", // alert类型 此参数无效
+  agreeFunc: function() { alert.show(); },
+  cancelFunc: function() { window.console.log("confirm: cancel"); } // alert类型 此方法无效
+});
+
+pop.show();
 
 // popover.model.set({title: "更新标题"});
