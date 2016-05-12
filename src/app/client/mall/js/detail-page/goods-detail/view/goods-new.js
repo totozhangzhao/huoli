e19b6c4457bca94537f33c36e69673b84c6aeb16
yujindong <83575615@qq.com>
2016-05-12 11:58:01 +0800

    fix 购买按钮文案

--- src/app/client/mall/js/detail-page/goods-detail/view/goods-new.js
+++ src/app/client/mall/js/detail-page/goods-detail/view/goods-new.js
@@ -206,12 +206,12 @@ var AppView = BaseView.extend({
 
   renderBuyNumView: function (goods) {
     this.buyNumModel.set({
-      type:0,
+      type: 0,
       hasMask: false,
       visible: true,
       title: "购买数量",
-      payText:goods.button,
-      payNumText: "去支付",
+      payText: goods.button,
+      payNumText: goods.button; //goods.money > 0 ? "去支付" : "立即兑换",
       points: goods.points,
       price: goods.money,
       limitNum: goods.limit,
