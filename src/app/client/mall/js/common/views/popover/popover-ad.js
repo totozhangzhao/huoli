7deac7b8437e8c3463233c5be401777d0441b1d8
yujindong <83575615@qq.com>
2016-06-24 10:38:08 +0800

    fix 弹窗广告

--- src/app/client/mall/js/common/views/popover/popover-ad.js
+++ src/app/client/mall/js/common/views/popover/popover-ad.js
@@ -1,11 +1,13 @@
 import Backbone   from "backbone";
+import _ from "lodash";
 import {sendPost} from "app/client/mall/js/lib/mall-request.js";
 import Storage    from "com/mobile/lib/storage/storage.js";
 
 import mallUitl from "app/client/mall/js/lib/util.js";
 import tplUtil from "app/client/mall/js/lib/mall-tpl.js";
-
-import "app/client/mall/js/lib/date-util.js";
+import * as mallPromise from "app/client/mall/js/lib/mall-promise.js";
+// 日期格式化
+// import "app/client/mall/js/lib/date-util.js";
 
 const storage       = new Storage("mallpop");
 const nativeStorage = require("app/client/mall/js/lib/storage.js");
@@ -22,16 +24,39 @@ const PopoverAd     = Backbone.View.extend({
   },
 
   fetch(param) {
-    this.param = param;
-    sendPost("getPopUp", param, (err, data) => {
-      if(err){
-        return;
-      }
-      if(data && data.length > 0) {
-        this.data = data[0];
-        this.render();
-      }
-    });
+    mallPromise
+      .getAppInfo()
+      .then(userData => {
+        const params = _.extend({}, param, userData.userInfo);
+        return new Promise((resovle, reject) => {
+          sendPost("getPopUp", params, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resovle(data);
+            }
+          });
+        });
+      })
+      .then(data => {
+        if(data && data.length > 0) {
+          this.data = data[0];
+          this.render();
+        }
+      })
+      .catch(mallPromise.catchFn);
+
+
+    // this.param = param;
+    // sendPost("getPopUp", param, (err, data) => {
+    //   if(err){
+    //     return;
+    //   }
+    //   if(data && data.length > 0) {
+    //     this.data = data[0];
+    //     this.render();
+    //   }
+    // });
 
   },
 
@@ -41,18 +66,22 @@ const PopoverAd     = Backbone.View.extend({
       data,
       tplUtil
     }));
-    this.store();
+    if(data.rule.type === 1) { // 后台控制
+      this.show();
+    }else{                     // 前端控制
+      this.store();
+    }
     return this;
   },
 
   store() {
     let namespace = "mallpop";
-    var now = new Date();
+    // var now = new Date();
     let key = `popid_${this.data.id}`;
     switch(this.data.rule.type) {
-      case 1:   // 每天弹n次
-        key = `popid_${now.format("yyyy-MM-dd")}_${this.data.id}`;
-        break;
+      // case 1:   // 每天弹n次
+        // key = `popid_${now.format("yyyy-MM-dd")}_${this.data.id}`;
+        // break;
       case 2:   // 每次重启弹n次
       default:
         key = `popid_${this.data.rule.type}_${this.data.id}`;
