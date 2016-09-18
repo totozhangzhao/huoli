77b5f397924236d3f6d8b7538432ed0a450e1953
yujindong <83575615@qq.com>
2016-09-18 10:51:12 +0800

    fix

--- src/app/client/mall/js/common/gift-config.js
+++ src/app/client/mall/js/common/gift-config.js
@@ -1,11 +1,17 @@
 // 微信送礼相关配置
 export default {
+  // 赠言
   defaultMessage: '精心准备了一份礼物，您喜欢不喜欢？开心收礼物啦\(^o^)/~',
+  // 订单状态
   OrderDetailInfo: {
     1: {className: 'send', tip: '未领取'},
     2: {className: 'notice', tip: '已领取'},
     3: {tip: '已被领取'},
     4: {className: 'again', tip: '已失效'},
     5: {className: 'again', tip: '未支付'}
+  },
+  // 分享信息
+  shareInfo:{
+    desc: '我精心准备了一份礼物，打开看看喜不喜欢？O(∩_∩)O~'
   }
 };
