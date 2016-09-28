6985a7359c7f6f587d18fa30d30f3a117d89d58b
yujindong <83575615@qq.com>
2016-09-28 11:43:46 +0800

    我的商城增加 loading

--- src/app/client/mall/js/profile/view/profile-index.js
+++ src/app/client/mall/js/profile/view/profile-index.js
@@ -5,6 +5,7 @@ var sendPost    = require("app/client/mall/js/lib/mall-request.js").sendPost;
 var mallPromise = require("app/client/mall/js/lib/mall-promise.js");
 var pageAction  = require("app/client/mall/js/lib/page-action.js");
 var cookie      = require("com/mobile/lib/cookie/cookie.js");
+var ui          = require("app/client/mall/js/lib/ui.js");
 import * as widget    from "app/client/mall/js/lib/common.js";
 
 
@@ -12,6 +13,7 @@ var AppView = Backbone.View.extend({
   el: "#profile-index",
   initialize(commonData) {
     _.extend(this, commonData);
+    this.$initial = ui.initial().show();
     this.fetchData();
   },
   resume() {
@@ -42,6 +44,7 @@ var AppView = Backbone.View.extend({
       .then((data) => {
         this.render(data);
         this.updateCouponCount();
+        this.$initial.hide();
       })
       .catch(mallPromise.catchFn);
   },
