9a1378f8353b640d7d2c7e9089e2668d529eb1f1
yujindong <83575615@qq.com>
2016-09-30 11:46:13 +0800

    fix

--- src/app/client/mall/js/common/gift-config.js
+++ src/app/client/mall/js/common/gift-config.js
@@ -1,7 +1,7 @@
 // 微信送礼相关配置
 export default {
   // 赠言
-  defaultMessage: '请收下我的礼物。小小心意，真挚祝福~~点一下就能领走哦！',
+  defaultMessage: '请收下我的礼物。小小心意，真挚祝福~点一下就能领走哦！',
   // 订单状态
   OrderDetailInfo: {
     1: {className: 'send', tip: '未领取'},
@@ -12,6 +12,6 @@ export default {
   },
   // 分享信息
   shareInfo:{
-    desc: '请收下我的礼物。小小心意，真挚祝福~~点一下就能领走哦！'
+    desc: '请收下我的礼物。小小心意，真挚祝福~点一下就能领走哦！'
   }
 };
