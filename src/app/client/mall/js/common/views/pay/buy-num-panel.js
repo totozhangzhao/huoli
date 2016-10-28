64a2c5d8de3595b2b0377b827e700c8813a08fa3
yujindong <83575615@qq.com>
2016-10-28 20:55:25 +0800

    fix bug

--- src/app/client/mall/js/common/views/pay/buy-num-panel.js
+++ src/app/client/mall/js/common/views/pay/buy-num-panel.js
@@ -36,14 +36,16 @@ const BuyNumPanelView = Backbone.View.extend({
   render() {
     if(this.model.get("specIndex") >= 0) {
       let specData = this.model.get("specList")[this.model.get("specIndex")];
-      this.model.set({
-        payType: specData.paytype,
-        points: specData.points,
-        price: specData.price,
-        specValueName: specData.spec,
-        specValueId: specData.goodspecid,
-        avatar: specData.img
-      }, { silent: true });
+      if(specData) {
+        this.model.set({
+          payType: specData.paytype,
+          points: specData.points,
+          price: specData.price,
+          specValueName: specData.spec,
+          specValueId: specData.goodspecid,
+          avatar: specData.img
+        }, { silent: true });
+      }
     }
     let tplData = this.model.toJSON();
     tplData.unitPriceText = this.model.getPPriceText(1);
